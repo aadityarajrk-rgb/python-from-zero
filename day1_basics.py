28d1d4b37bcea7506aa298e7b0065cad239828a5
Aditya Raj <aadityarajrk@gmail.com>
2026-01-24 23:25:59 +0530

    Add Day 1 Python basics
    Learned :- printing , variables ,and data types

---
+++ day1_basics.py
@@ -0,0 +1,36 @@
+#Day1: python basics
+
+# Printing text :
+
+print("hellow world")
+print("my name is Aditya raj")
+print(16)
+print("my name is aditya raj","i am 16 years old")
+
+# Variables
+
+name="aditya raj"
+age=16
+print(name)
+print(age)
+age2=age
+print("my name is:",name)
+print("my age is:",age2)
+price=2.11
+a=True
+b=None
+
+# Data types
+
+print(type(name))  #string
+print(type(age))   #int.
+print(type(price)) #float.
+print(type(a))     #bool
+print(type(b))     #nonetype
+
+# Simple operation
+
+a=2
+b=5
+print(a+b)
+print(a-b)
