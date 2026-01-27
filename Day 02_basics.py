a2f48bde8e979f382a8b5a1be1c4470090f86943
Aditya Raj <aadityarajrk@gmail.com>
2026-01-27 22:28:35 +0530

    Day 2 basics.py
    learned: operators and its types

---
+++ Day 02_basics.py
@@ -0,0 +1,50 @@
+                                                         # Day 2 : Python basics
+
+# Types of operators
+
+# # 1 : Arithmetic operators
+# a=5
+# b=4
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)  #always in flooting
+# print(a%b)  #remainder
+# print(a**b) #a^b
+
+# # Relational /comparison operator(ask question for comparision gives output in True/Falsr)
+# a=50
+# b=20
+
+# print(a==b)   #is a equal to b = False
+# print(a!=b)    # is a  not equal to b = True
+# print(a>=b)    # is a  greater and equal to b = True
+# print(a>b)     #True
+# print(a<=b)    #is a smaller and equal to b = False
+# print(a<b)     # False
+
+# Assignment operators
+# num=10     
+# num+=10     # 10+10 = 20/num=num+10
+# print("num:",num)
+# num-= 10    # 10-10 =0
+# print("num:",num)
+# num/=5      # 10/5 = 2
+# print("num:",num)
+# num%=5      # 2
+# print("num:",num)
+# num**=5     # 10^5 = 100000
+# print("num:",num)
+
+# Logical operators(not , and , or)
+a=50
+b=20
+print(not True)
+print(not False)
+print(not(a>b))
+
+val1=False  #if true then output will be true 
+val2=True   #if false then output will be false
+print("and operator:",val1 and val2)  #if any of value is false then output will be false or vice versa
+
+print("or operator:",val1 or val2)
