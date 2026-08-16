76a682904792f3a10cef9f8d40a30da5274479b3
Aditya Raj <aadityarajrk@gmail.com>
2026-08-17 00:00:47 +0530

    Add files via upload
    some more  basics about "print function"

---
+++ Day 03-basics.py
@@ -0,0 +1,55 @@
+print("hi python")
+
+
+print ("  -------------------")
+print ("      hello aditya ")
+print ("  -------------------")
+
+
+# \" escape double quote
+# allows you to include a " without ending 
+print (" hi \"python\"") 
+print ('hi "python"')
+print ('hi \'python\'')
+
+# \\ escape backslash
+# use real backslash \ not as start of an escape sequence 
+print("path :c:\\user\\ind")
+
+print("message 1")
+print("message 2")
+print("______________")
+print("message 1")
+print()  # empty line for space 
+print("message 2")
+
+# \n escape new line 
+# adds nanenw line , it moves the test that comes after to the next line 
+
+print("message1\n")
+print ("message 2")
+
+
+# for three lines
+
+print("message1\n\n\n")
+print ("message 2")
+# -------------------------------
+print("message 1\n\n\nmessage2")
+print ("message1\nmessage2")  #no space b\m them
+       
+
+# \t escape Tab
+# adds a tab space
+
+print("messsage1\tmessage2")
+
+# practice
+
+print("your learning path\n\t - python basics \n\t - dat engineering \n\t - ai ")
+
+
+print("""your learning path 
+\t-python basics
+\t-data engineering 
+\t-ai""")   # triple qoutes :- allows us to write text in multiple line string 
