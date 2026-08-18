eba996500c8edee3da48f603c384a6944fb57e5b
Aditya Raj <aadityarajrk@gmail.com>
2026-08-18 19:30:32 +0530

    Add files via upload
    learned about input function and data type

---
+++ Day 04-basics.py
@@ -0,0 +1,26 @@
+
+# input function
+
+input ("enter your name:")
+
+# input and variables :- using input () alone reads the users response but immediately discards it ,to keep the value , assign it to avariable 
+
+name=input ("enter your name ")  # dynamic values:- date entered by the user that can vary each time the program run
+country="germany"   # hard coded (static) value :- fixed piece of dat a weitten  directly into your code  that never changes at runtime 
+
+print (name,"comes from", country)
+# input makes  your programm interactive
+# input lets your program ask questions and react to what the user types making it feel alive 
+
+
+# data types
+a=10  #integer
+b=3.15 #float
+c="hello" #string
+d='hi'
+e="1234" #str
+f= True #boolean : can be either true or false used to handle logic and decision making
+g=False #bool  : they must start with capital letter
+h=None  #NoneType:- means"no value",  nothing or unknown used to show the absenceof any data
+i=""   # str-blank :-blank""is a string value with no characters inside it is not same as None 
+j=" "   # str - empty space :-white  space " "is a string value with 1 or more spaces , it is not same as None
