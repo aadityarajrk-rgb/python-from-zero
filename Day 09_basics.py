697e480848381106b3db65261b69ee340566881c
Aditya Raj <aadityarajrk@gmail.com>
2026-09-06 13:38:56 +0530

    Add files via upload
    learnt about : types of numeric
    int, float,complex,str,type

---
+++ Day 09_basics.py
@@ -0,0 +1,28 @@
+# working with numbers
+# number function type
+x=5
+y=5.7
+z=2+3j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+a="25"
+print(type(a))
+# int(value) built in function, output:int
+# converts compatible value into int value
+a=int(a)
+print(type(x))
+print(a*3)
+
+# float(value) built in function, output: float
+# converts compatible value into float value 
+x="3"
+print(float(x))
+
+# complex(real,imaginary) built in function, output:complex 
+# creates a complex number using real and imaginary parts
+x=3 #real 
+y=4 # imaginary
+print(complex(x,y))
