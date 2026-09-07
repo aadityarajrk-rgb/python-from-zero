c48eaa6ae0ffdb319367169eca22fd711836424b
Aditya Raj <aadityarajrk@gmail.com>
2026-09-07 08:27:38 +0530

    Add files via upload
    learnt about string function=validating

---
+++ Day 10_basics.py
@@ -0,0 +1,9 @@
+# String function= Validating
+country="india"
+# check if the country name contains only letter
+# isalpha()  ,str method , output:boolean = checks if the string has only letter
+print(country.isalpha())
+
+# isnumeric()  ,str method  , output:boolean= checks if the string has only numbers
+phone="1234567890"
+print(phone.isnumeric())
